e29a2ab4739d5d65c0ae9ee0a79013a0ee3c9f68
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-03-03 14:22:55 +0100

    Add Container

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,8 +5,10 @@ import Button from '../Button/Button';
 const SearchForm = () => {
   return (
       <form className={styles.searchForm}>
-          <TextInput placeholder="Search"/>
-          <Button text="Search"/>
+          <TextInput placeholder="Search..."/>
+          <Button>
+            <span className='fa fa-search'></span>
+          </Button>
       </form>
   );
 };
