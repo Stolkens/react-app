9ed7b4ba9269f1f239ad6325f06cee74cfdd569b
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-03-15 08:35:03 +0100

    Add Card & CardForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -6,15 +6,17 @@ import TextInput from '../TextInput/TextInput';
 
 
 const ColumnForm = props => {
-  const [title, setTitle] = useState('');
-  const [icon, setIcon] = useState('')
+  const [title, setTitle] = useState(''); // tworzy stan dla inputa i przypisuje mu wartość początkową '' (pusty string)
+  const [icon, setIcon] = useState(''); // tworzy stan dla inputa i przypisuje mu wartość początkową '' (pusty string)
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    props.action({ title: title, icon: icon });
-    setTitle('');
+  const handleSubmit = (e) => {
+    // tworzy funkcję, która będzie wywoływana po kliknięciu w przycisk
+    e.preventDefault(); // zapobiega przeładowaniu strony po kliknięciu w przycisk
+    props.action({ title: title, icon: icon }); // wywołuje funkcję przekazaną w propsach, która dodaje nową kolumnę do tablicy kolumn w App.js
+    setTitle(''); // czyści pole input po dodaniu nowej kolumny
     setIcon('');
-}
+  };
+
   return (
     <form className={styles.columnForm} onSubmit={handleSubmit}>
       <span>Title:</span><TextInput value={title} onChange={e => setTitle(e.target.value)} />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+const container = document.querySelector('#root')
+const root = createRoot(container)
+
+root.render(
+  <App tab="home"/>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
