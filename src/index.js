df51a1c580c5729c022057454e7a9db4344c48ec
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-04-17 15:18:19 +0200

    Add redux, store.js, initialState

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -8,8 +10,9 @@ const container = document.querySelector('#root')
 const root = createRoot(container)
 
 root.render(
-  <App tab="home"/>
-  
+  <Provider store={store}>
+    <App tab="home"/>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
