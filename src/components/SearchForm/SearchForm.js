aa599eea10eedc5ade6666aa69f7ed5dbd7b5659
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-04-27 10:16:45 +0200

    Fix SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,7 +2,7 @@ import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { searchString } from '../../redux/store';
 
 
@@ -18,8 +18,11 @@ const handleSubmit = e => {
   // dispatch({type: 'SEARCHGSTRING', payload: searchCard })
   
   setSearchCard('')
-
 };
+
+useEffect(() => { // useEffect wykonuje się po każdym renderze komponentu
+  dispatch(searchString('')); // wywołuje akcję searchString i przekazuje do niej pusty string
+}, [dispatch]); // drugi parametr to tablica zależności, która określa, kiedy useEffect ma się wykonać
   
   return (
       <form className={styles.searchForm} onSubmit={handleSubmit}>
