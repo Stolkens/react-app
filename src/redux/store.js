591d163065af44b4992852bfc63d2615f875e5ed
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-04-28 10:18:19 +0200

    Add dynamic class isFavorite and style

--- src/redux/store.js
+++ src/redux/store.js
@@ -23,6 +23,8 @@ export const getAllLists = (state) => state.lists;
 
 export const addList = (payload) =>({type: 'ADD_LIST', payload});
 
+export const toggleCardFavorite = (payload) => ({type: 'TOGGLE_CARD_FAVORITE', payload});
+
 const reducer = (state, action) => {
   switch(action.type) {
     case 'ADD_COLUMN':
@@ -31,8 +33,10 @@ const reducer = (state, action) => {
       return { ...state, cards: [...state.cards, {id: shortid(),...action.payload}]};
     case 'SEARCHSTRING':
       return { ...state, searchString: action.payload};
-      case 'ADD_LIST':
-        return { ...state, lists: [...state.lists, {id:shortid(),...action.payload}]};
+    case 'ADD_LIST':
+      return { ...state, lists: [...state.lists, {id:shortid(),...action.payload}]};
+    case 'TOGGLE_CARD_FAVORITE':
+      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card) };
     default:
       return state;
   }
