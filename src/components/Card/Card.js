2ab4d346fc938a8c49871d5ddb09e6a229c2c3de
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-05-01 07:39:37 +0200

    Add REMOVE_CARD action

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,7 @@ import styles from "./Card.module.scss";
 import clsx from "clsx";
 import { useDispatch } from "react-redux";
 import { toggleCardFavorite } from "../../redux/cardsReducer";
+import { removeCard } from "../../redux/cardsReducer";
 
 
 const Card = props => {
@@ -12,11 +13,19 @@ const Card = props => {
     e.preventDefault();
     dispatch(toggleCardFavorite(props.id)) 
   }
+
+  const handleDelete=(e)=> {
+    e.preventDefault();
+    dispatch(removeCard(props.id))
+  }
  
   return (
     <li className={styles.card}>{props.title}
-            <button onClick={handleClick} className={clsx(styles.button, props.isFavorite && styles.isActive)}><i className={'fa fa-star-o'} /></button>
-        </li> 
+      <div>
+        <button onClick={handleClick} className={clsx(styles.button, props.isFavorite && styles.isActive)}><i className={'fa fa-star-o'} /></button>
+        <button onClick={handleDelete} className={styles.button}><i className={'fa fa-trash'} /></button>
+      </div>        
+    </li> 
     )
 };
 
--- src/redux/cardsReducer.js
+++ src/redux/cardsReducer.js
@@ -1,12 +1,14 @@
 import shortid from "shortid";
 const ADD_CARD = 'app/lists/ADD_CARD';
-const TOGGLE_CARD_FAVORITE = 'app/lists/TOGGLE_CARD_FAVORITE'
+const TOGGLE_CARD_FAVORITE = 'app/lists/TOGGLE_CARD_FAVORITE';
+const REMOVE_CARD = 'app/lists/REMOVE_CARD';
 
 // selectors
 export const getFavoriteCards = (state) =>state.cards.filter(card=>card.isFavorite === true);
 
 //actions
 export const addCard = (payload) => ({type: ADD_CARD, payload})
+export const removeCard = (payload) => ({type: REMOVE_CARD, payload})
 export const toggleCardFavorite = (payload) => ({type: TOGGLE_CARD_FAVORITE, payload});
 
 const cardsReducer = (statePart = [], action) => {
@@ -15,9 +17,12 @@ const cardsReducer = (statePart = [], action) => {
       return [...statePart, { ...action.payload, id: shortid() }];
     case TOGGLE_CARD_FAVORITE:
       return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+    case REMOVE_CARD:
+      return statePart.filter(card =>card.id !== action.payload); 
     default:
       return statePart;
   }
 };
 
 export default cardsReducer;
+
